57868a90cee660692229e43b215a60650a0ba6f7
Alec McClure <sabotaione@gmail.com>
2018-05-01 19:55:58 -0400

    added in motion blur and some new minor scripts

---
+++ Assets/Scripts/Cheat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cheat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Alpha1))
+			SceneManager.LoadScene(0);
+		if (Input.GetKey(KeyCode.Alpha2))
+			SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Scripts/ShameGame/SwapScripts.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapScripts : MonoBehaviour {
+	public EyePlayer script1, script2;
+	public KeyCode actionKey;
+	KeyCode fwdKey, lKey, rKey, backKey;
+	public bool defaultSwap = true;
+	// Use this for initialization
+	void Start () {
+
+		fwdKey = script2.fwdKey;
+		lKey = script2.lKey;
+		rKey = script2.rKey;
+		backKey = script2.backKey;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (defaultSwap){
+			if (Input.GetKey(actionKey)){
+				script2.fwdKey = fwdKey;
+				script2.lKey = lKey;
+				script2.rKey = rKey;
+				script2.backKey = backKey;
+			} else {
+				script2.fwdKey = script1.fwdKey;
+				script2.lKey = script1.lKey;
+				script2.rKey = script1.rKey;
+				script2.backKey = script1.backKey;
+
+			}
+
+			} else {
+		if (Input.GetKey(actionKey)){
+			script2.fwdKey = script1.fwdKey;
+			script2.lKey = script1.lKey;
+			script2.rKey = script1.rKey;
+			script2.backKey = script1.backKey;
+
+		} else {
+			script2.fwdKey = fwdKey;
+			script2.lKey = lKey;
+			script2.rKey = rKey;
+			script2.backKey = backKey;
+		}
+			}
+
+	}
+}
