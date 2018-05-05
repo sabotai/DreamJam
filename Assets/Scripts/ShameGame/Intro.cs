765f061aeec8f907e675d25abe2ef8d7168a4411
Juno A. McClure <sabotaione@gmail.com>
2018-05-04 20:13:37 -0400

    text going now in the beginning, intro scene doing well

---
+++ Assets/Scripts/ShameGame/Intro.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour {
+
+	public GameObject bag;
+	public GameObject insideLight;
+	public RawImage canvasImage;
+	int state = 0;
+	public float pct = 1f;
+	AudioSource audSrc;
+	public AudioClip clipSwitch, clipBag, clipDissolve;
+	public Material sky;
+	public Color origSky;
+	public Light dirLight;
+	public GameObject[] textObjects;
+	// Use this for initialization
+	void Start () {
+		audSrc = GetComponent< AudioSource>();
+		sky.color = new Color(245f/255f, 78f/255f, 78f/255f, 1f);
+		origSky = sky.color;
+		dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
+
+		state -= textObjects.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state > -textObjects.Length && state < 1){
+			textObjects[textObjects.Length + state - 1].SetActive(true);
+		} else if (state == 1){
+			for (int i = 0; i < textObjects.Length; i++){
+				textObjects[i].SetActive(false);
+			}
+		} else if(state == 2){
+			insideLight.SetActive(false);
+			if (audSrc.clip != clipSwitch){
+				audSrc.clip = clipSwitch;
+				audSrc.Play();
+			}
+		} else if (state == 3){
+			bag.SetActive(false);
+			if (audSrc.clip != clipBag){
+			audSrc.clip = clipBag;
+			audSrc.Play();
+		}
+		} else if (state >= 4){
+			if (audSrc.clip != clipDissolve){
+			audSrc.clip = clipDissolve;
+			audSrc.Play();
+		}
+			updateRes(2, 1, 1024, 576, pct);
+			//sky.SetColor("_MainColor", Color.Lerp(Color.red, sky.GetColor("_MainColor"), pct));
+			sky.color = Color.Lerp(Color.red, origSky, pct);
+			dirLight.color = sky.color;
+			pct -= (Time.deltaTime / 2f);
+			Debug.Log("pct = " + pct);
+
+		}
+		if (Input.GetKeyDown(KeyCode.Space)){
+			state++;
+		}
+
+		if (pct < 0f) {
+			sky.color = origSky;
+			dirLight.color = sky.color;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+		}
+	}
+
+		void updateRes(float minWidth, float minHeight, float maxWidth, float maxHeight, float pct){
+
+		float newW = Mathf.Lerp(minWidth, maxWidth, pct);
+		float newH = Mathf.Lerp(minHeight, maxHeight, pct);
+		
+		//update resolution of render textures
+		RenderTexture newRT = new RenderTexture( (int)newW, (int)newH, 16, RenderTextureFormat.ARGBFloat );
+		newRT.filterMode = FilterMode.Point;
+		Camera.main.targetTexture = newRT;
+		canvasImage.texture = newRT;
+
+	}
+}
---
+++ Assets/Scripts/ShameGame/PixelIntroOutro.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PixelIntroOutro : MonoBehaviour {
+
+	public RawImage[] canvasImages;
+	public Camera[] cams;
+	public float pct = 1f;
+	public bool intro = true;
+	public float minWidth = 16;
+	public float minHeight = 9;
+	public float maxWidth = 512;
+	public float maxHeight = 288;
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		if (intro) pct = 0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pct < 1f){
+			updateRes(minWidth, minHeight, maxWidth, maxHeight, pct);
+			pct += Time.deltaTime * speed;
+		} else {
+			enabled = false;
+		}
+	}
+
+
+	void updateRes(float _minWidth, float _minHeight, float _maxWidth, float _maxHeight, float _pct){
+
+		float newW = Mathf.Lerp(_minWidth, _maxWidth, _pct);
+		float newH = Mathf.Lerp(_minHeight, _maxHeight, _pct);
+		
+		//update resolution of render textures
+		RenderTexture newRT = new RenderTexture( (int)newW, (int)newH, 16, RenderTextureFormat.ARGBFloat );
+		newRT.filterMode = FilterMode.Point;
+
+		for (int i = 0; i < cams.Length; i++){
+			cams[i].targetTexture = newRT;
+			canvasImages[i].texture = newRT;
+		}
+
+	}
+}
