1c41ed0757d530e6bc514678b6a8eeabf14e9fab
sabotai <sabotaione@gmail.com>
2018-05-20 18:10:49 -0400

    new title, 2 new stages for orbtown

---
+++ Assets/Scripts/ExtendedPulse.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExtendedPulse : MonoBehaviour {
+
+	public Color cOn, cOff;
+	public int dir;
+	float current;
+	public float speed = 1f;
+	float initSpeed;
+	float maxSpeed = 8f;
+	public bool ui;
+	public float sleepTime = 10f;
+	public float awakeTime = 5f;
+	float startTime;
+	float timerAmt;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		current = 0f;
+		if (dir == 0) dir = 1;
+		else if (dir == -1) current = 1f;
+		initSpeed = speed;
+		timerAmt = awakeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ui){
+				GetComponent<Text>().color = Color.Lerp(cOn, cOff, current);
+			} else {
+				GetComponent<Renderer>().material.color = Color.Lerp(cOn, cOff, current);
+			}
+			current += (speed * dir * Time.deltaTime);
+
+
+			if (startTime + timerAmt < Time.time){
+				if (current > 1f || current < 0f) {
+					startTime += timerAmt;
+					dir *= -1;
+
+					if (current > 1f) timerAmt = awakeTime;
+					if (current < 0f) timerAmt = sleepTime;
+				} 
+				
+			}
+
+			current = Mathf.Clamp(current, 0f, 1f);
+		}
+	
+}
---
+++ Assets/Scripts/RandomStageSetup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStageSetup : MonoBehaviour {
+	Camera myCam;
+	public Platform myPlat;
+	bool flash = false;
+	public Color c, c1, c2;
+	public bool randomizeColor = true;
+	public bool randomizePlats = true;
+	public bool randomizeTextures = false;
+	public Texture[] texts;
+	public Renderer[] randoRends;
+
+	// Use this for initialization
+	void Start () {
+		float rando = Random.value;
+		if (randomizeColor){
+			if (c1 == null) c1 = Color.red;
+			if (c2 == null) c2 = Color.blue;
+
+			myCam = GetComponent<Camera>();
+			c = Color.Lerp(c1, c2, rando);
+			myCam.backgroundColor = c;
+
+			rando = Random.value;
+			if (rando > 0.69f) flash = true;
+
+			if (flash) {
+				rando = Random.value;
+				c1 = Color.Lerp(c1, c2, rando);
+				rando = Random.value;
+				c2 = Color.Lerp(c1, c2, rando);
+			}
+		}
+		if (randomizePlats){
+			rando = Random.value;
+			if (rando >= 0.25f){
+				if (rando >= 0.75f) myPlat.randomize = true; else myPlat.waves = true;
+			}
+		}
+
+		if (randomizeTextures){
+			for (int i = 0; i < randoRends.Length; i++){
+				randoRends[i].material.mainTexture = texts[Random.Range(0, texts.Length)];
+
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flash){
+			float pct = Mathf.PingPong(Time.time / 2f, 1);
+			c = Color.Lerp(c1, c2, pct);
+			myCam.backgroundColor = c;
+		}
+	}
+}
